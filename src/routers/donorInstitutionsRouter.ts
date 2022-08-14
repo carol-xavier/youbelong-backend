380770bae1cd8fec7674f97983942c5096c62af4
carol-xavier <xavier.biotec@gmail.com>
2022-08-14 17:36:56 -0300

    PUT/donor route created to save institutions for specific donor/user

--- src/routers/donorInstitutionsRouter.ts
+++ src/routers/donorInstitutionsRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import {verifyToken} from "./../middlewares/tokenMiddleware.js";
+import { updateDonorInstitutions } from "../controllers/donorInstitutionsController.js";
 
 const donorInstitutionsRouter = Router();
-donorInstitutionsRouter.use(verifyToken);
+
+donorInstitutionsRouter.put("/donor", verifyToken, updateDonorInstitutions);
 
 export default donorInstitutionsRouter;
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRouter from "./authRouter.js";
 import institutionRouter from "./institutionRouter.js";
 import articleRouter from "./articleRouter.js";
+import donorInstitutionsRouter from "./donorInstitutionsRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
 router.use("/institutions", institutionRouter);
-router.use(articleRouter)
+router.use(articleRouter);
+router.use(donorInstitutionsRouter);
 
 export default router;
